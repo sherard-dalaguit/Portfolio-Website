5bdf86f966a18cad42b3a3bfa402f3dbe5f2e306
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-21 19:51:51 -0700

    Add click handler to MagicButton and new gradient component
    Enhanced the `MagicButton` component by adding an `onClick` handler to increase interactivity. Modified background classes in `Hero` and `Home` components for consistent styling. Introduced a `BackgroundGradientAnimation` component to create dynamic gradient backgrounds with rich customization options.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
             <div className="h-screen w-full dark:bg-black bg-white dark:bg-grid-white/[0.075] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
                 {/* Radial gradient for the container to give a faded look */}
-                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+                <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
             </div>
 
             <div className="flex justify-center relative my-20 z-10">
