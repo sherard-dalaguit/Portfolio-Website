347cfbc42adcc4d0e543798a1213325aa08b399b
sherard_dalaguit <sherard.softwaredev@gmail.com>
2024-09-21 19:51:32 -0700

    Update Grid component and refine BentoGrid structure
    Revised the Grid component to include additional properties and enhanced styling. Removed the login button from the floating navbar. Extended BentoGridItem with new properties and updated its layout for better visual presentation.

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -4,9 +4,19 @@ import {gridItems} from "@/data";
 const Grid = () => {
     return (
         <section id="about">
-            <BentoGrid>
-                {gridItems.map((item) => (
-                    <BentoGridItem id={item.id} key={item.id} title={item.title} description={item.description} />
+            <BentoGrid className="w-full py-20">
+                {gridItems.map(({id, title, description, className, img, imgClassName, titleClassName, spareImg}) => (
+                    <BentoGridItem
+                        id={id}
+                        key={id}
+                        title={title}
+                        description={description}
+                        className={className}
+                        img={img}
+                        imgClassName={imgClassName}
+                        titleClassName={titleClassName}
+                        spareImg={spareImg}
+                    />
                 ))}
             </BentoGrid>
         </section>
